352f05ba4218f28ba03a9720738a005dd7ffd299
Andrey Nerezko <ane@bam-boo.eu>
2021-07-13 21:32:05 +0300

    init commit

---
+++ intimatica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intimatica
+//
+//  Created by Andrey RustFox on 7/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
